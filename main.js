b6901c7e559c709b499e381c8dea4a321e31497c
CatAvadani <catalinaavadani@gmail.com>
2023-11-15 21:15:14 +0100

    Finished project

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
 window.addEventListener("DOMContentLoaded", main);
 
-let currentImgIndex = 0;
-
 function main() {
-  beginSlideShow();
+  beginSlideShow("#slideshow-1");
+
+  beginSlideShow("#slideshow-2");
 }
-function beginSlideShow() {
+
+function beginSlideShow(selector) {
+  let currentImgIndex = 0;
   setInterval(showNextImage, 2000);
-}
 
-function showNextImage() {
-  // get all the images from dom
-  const images = document.querySelectorAll(".slideshow-container img");
+  function showNextImage() {
+    // get all the images from dom
+    const images = document.querySelectorAll(selector + " img");
 
-  // hide images which are visible now
-  const visibleImage = images[currentImgIndex];
-  visibleImage.classList.remove("show");
+    // hide images which are visible now
+    const visibleImage = images[currentImgIndex];
+    visibleImage.classList.remove("show");
 
-  //   currentImgIndex = (currentImgIndex + 1) % images.length;
-  // set the next img index which needs to be seen
-  currentImgIndex++;
-  if (currentImgIndex >= images.length) {
-    currentImgIndex = 0;
+    //   currentImgIndex = (currentImgIndex + 1) % images.length;
+    // set the next img index which needs to be seen
+    currentImgIndex++;
+    if (currentImgIndex >= images.length) {
+      currentImgIndex = 0;
+    }
+
+    // show the image which is hidden now.
+    const hiddenImage = images[currentImgIndex];
+    hiddenImage.classList.add("show");
+    console.log(visibleImage);
   }
-
-  // show the image which is hidden now.
-  const hiddenImage = images[currentImgIndex];
-  hiddenImage.classList.add("show");
-  console.log(visibleImage);
 }
